348c4c1bc03c690e34c0345759d9d5e0dc663927
Dennis Ego <dennis.ego@artcoresociety.com>
2025-01-16 15:51:11 +0100

    Add ocean subsurface mesh

--- src/js/Classes/Scene.js
+++ src/js/Classes/Scene.js
@@ -1,8 +1,10 @@
 import {
+	BoxGeometry,
 	Color,
 	DoubleSide,
 	EquirectangularReflectionMapping,
 	Mesh,
+	MeshBasicMaterial,
 	PlaneGeometry,
 	ShaderMaterial,
 	Uniform,
@@ -16,12 +18,17 @@ class Scene extends ThreeManager {
 	constructor() {
 		super();
 
-		this.oceanMaterial = null;
+		this.oceanSurfaceMaterial = null;
 		this.config = {
 			depthColor: '#186691',
 			surfaceColor: '#9bd8ff',
 			fogColor: '#ffffff',
-			foamColor: '#ffffff'
+			foamColor: '#ffffff',
+			dimensions: {
+				height: 150,
+				width: 150,
+				depth: 150
+			}
 		};
 	}
 
@@ -35,7 +42,7 @@ class Scene extends ThreeManager {
 		// Set render action
 		this.setRenderAction(() => {
 			// Update the uniforms
-			this.oceanMaterial.uniforms.uTime.value = this.clock.getElapsedTime();
+			this.oceanSurfaceMaterial.uniforms.uTime.value = this.clock.getElapsedTime();
 		});
 
 		// Start animation loop
@@ -50,7 +57,7 @@ class Scene extends ThreeManager {
 
 	addOcean() {
 		// Creat ocean shader material
-		this.oceanMaterial = new ShaderMaterial({
+		this.oceanSurfaceMaterial = new ShaderMaterial({
 			vertexShader: oceanVertexShader,
 			fragmentShader: oceanFragmentShader,
 			uniforms: {
@@ -83,82 +90,87 @@ class Scene extends ThreeManager {
 			// Add debug controls
 			this.addDebugControls(() => {
 				this.gui
-					.add(this.oceanMaterial.uniforms.uBigWavesElevation, 'value')
+					.add(this.oceanSurfaceMaterial.uniforms.uBigWavesElevation, 'value')
 					.min(0)
 					.max(1)
 					.step(0.001)
 					.name('uBigWavesElevation');
 				this.gui
-					.add(this.oceanMaterial.uniforms.uBigWavesFrequency.value, 'x')
+					.add(this.oceanSurfaceMaterial.uniforms.uBigWavesFrequency.value, 'x')
 					.min(0)
 					.max(10)
 					.step(0.001)
 					.name('uBigWavesFrequencyX');
 				this.gui
-					.add(this.oceanMaterial.uniforms.uBigWavesFrequency.value, 'y')
+					.add(this.oceanSurfaceMaterial.uniforms.uBigWavesFrequency.value, 'y')
 					.min(0)
 					.max(10)
 					.step(0.001)
 					.name('uBigWavesFrequencyY');
 				this.gui
-					.add(this.oceanMaterial.uniforms.uBigWavesSpeed, 'value')
+					.add(this.oceanSurfaceMaterial.uniforms.uBigWavesSpeed, 'value')
 					.min(0)
 					.max(4)
 					.step(0.001)
 					.name('uBigWavesSpeed');
 
 				this.gui
-					.add(this.oceanMaterial.uniforms.uSmallWavesElevation, 'value')
+					.add(this.oceanSurfaceMaterial.uniforms.uSmallWavesElevation, 'value')
 					.min(0)
 					.max(1)
 					.step(0.001)
 					.name('uSmallWavesElevation');
 				this.gui
-					.add(this.oceanMaterial.uniforms.uSmallWavesFrequency, 'value')
+					.add(this.oceanSurfaceMaterial.uniforms.uSmallWavesFrequency, 'value')
 					.min(0)
 					.max(30)
 					.step(0.001)
 					.name('uSmallWavesFrequency');
 				this.gui
-					.add(this.oceanMaterial.uniforms.uSmallWavesSpeed, 'value')
+					.add(this.oceanSurfaceMaterial.uniforms.uSmallWavesSpeed, 'value')
 					.min(0)
 					.max(4)
 					.step(0.001)
 					.name('uSmallWavesSpeed');
 				this.gui
-					.add(this.oceanMaterial.uniforms.uSmallIterations, 'value')
+					.add(this.oceanSurfaceMaterial.uniforms.uSmallIterations, 'value')
 					.min(0)
 					.max(5)
 					.step(1)
 					.name('uSmallIterations');
 
 				this.gui.addColor(this.config, 'depthColor').onChange(() => {
-					this.oceanMaterial.uniforms.uDepthColor.value.set(this.config.depthColor);
+					this.oceanSurfaceMaterial.uniforms.uDepthColor.value.set(this.config.depthColor);
 				});
 				this.gui.addColor(this.config, 'surfaceColor').onChange(() => {
-					this.oceanMaterial.uniforms.uSurfaceColor.value.set(this.config.surfaceColor);
+					this.oceanSurfaceMaterial.uniforms.uSurfaceColor.value.set(this.config.surfaceColor);
 				});
-				this.gui.add(this.oceanMaterial.uniforms.uColorOffset, 'value').min(0).max(1).step(0.001).name('uColorOffset');
 				this.gui
-					.add(this.oceanMaterial.uniforms.uColorMultiplier, 'value')
+					.add(this.oceanSurfaceMaterial.uniforms.uColorOffset, 'value')
+					.min(0)
+					.max(1)
+					.step(0.001)
+					.name('uColorOffset');
+				this.gui
+					.add(this.oceanSurfaceMaterial.uniforms.uColorMultiplier, 'value')
 					.min(0)
 					.max(10)
 					.step(0.001)
 					.name('uColorMultiplier');
 				this.gui.addColor(this.config, 'fogColor').onChange(() => {
-					this.oceanMaterial.uniforms.uFogColor.value.set(this.config.fogColor);
+					this.oceanSurfaceMaterial.uniforms.uFogColor.value.set(this.config.fogColor);
 				});
 				this.gui
-					.add(this.oceanMaterial.uniforms.uFogIntensity, 'value')
+					.add(this.oceanSurfaceMaterial.uniforms.uFogIntensity, 'value')
 					.min(0)
 					.max(10)
 					.step(0.001)
 					.name('uFogIntensity');
 				this.gui.addColor(this.config, 'foamColor').onChange(() => {
-					this.oceanMaterial.uniforms.uFoamColor.value.set(this.config.foamColor);
+					this.oceanSurfaceMaterial.uniforms.uFoamColor.value.set(this.config.foamColor);
 				});
 				this.gui
-					.add(this.oceanMaterial.uniforms.uFoamIntensity, 'value')
+					.add(this.oceanSurfaceMaterial.uniforms.uFoamIntensity, 'value')
 					.min(0)
 					.max(1)
 					.step(0.001)
@@ -167,16 +179,39 @@ class Scene extends ThreeManager {
 		}
 
 		// Create ocean geometry
-		const oceanGeometry = new PlaneGeometry(150, 150, 512, 512);
+		const oceanSurfaceGeometry = new PlaneGeometry(
+			this.config.dimensions.width,
+			this.config.dimensions.height,
+			512,
+			512
+		);
 
-		// Create ocean mesh
-		const ocean = new Mesh(oceanGeometry, this.oceanMaterial);
+		// Create ocean surface mesh
+		const oceanSurface = new Mesh(oceanSurfaceGeometry, this.oceanSurfaceMaterial);
 
-		// Adjust ocean
-		ocean.rotation.x = -Math.PI / 2;
+		// Adjust the ocean surface mesh
+		oceanSurface.rotation.x = -Math.PI / 2;
 
 		// Add to scene
-		this.scene.add(ocean);
+		this.scene.add(oceanSurface);
+
+		// Add ocean subsurface geometry below the ocean
+		const oceanSubsurfaceGeometry = new BoxGeometry(
+			this.config.dimensions.width,
+			this.config.dimensions.height,
+			this.config.dimensions.depth
+		);
+		const oceanSubsurfaceMaterial = new ShaderMaterial({ side: DoubleSide });
+		const oceanSubsurface = new Mesh(oceanSubsurfaceGeometry, oceanSubsurfaceMaterial);
+
+		// Translate the geometry to align the bottom at the ocean plane (0, 0, 0)
+		// Move down by half the height to position the bottom of the box at y = 0
+		// Origin is by default at center
+		const offsetHeight = this.config.dimensions.height + 0.45;
+		oceanSubsurfaceGeometry.translate(0, -offsetHeight / 2, 0);
+
+		// Add the ocean subsurface to the scene
+		this.scene.add(oceanSubsurface);
 	}
 
 	addSky() {
