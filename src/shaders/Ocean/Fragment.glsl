04001341cb6bfca3161e5002fbc4dc6835f9bc37
Dennis Ego <dennis.ego@artcoresociety.com>
2025-01-16 12:29:07 +0100

    Add comments to shader files

--- src/shaders/Ocean/Fragment.glsl
+++ src/shaders/Ocean/Fragment.glsl
@@ -11,14 +11,20 @@ varying vec2 vUv;
 
 void main()
 {
+    // Calculate the distance from the edge of the plane using UV coordinates
     float edgeDistance = min(vUv.x, 1.0 - vUv.x);
     edgeDistance = min(edgeDistance, min(vUv.y, 1.0 - vUv.y));
 
+    // Inverse the edgeDistance to create a fog effect that starts at 0 in the center and increases towards 1 at the edges
     float fogFactor = 1.0 - clamp(edgeDistance, 0.0, 1.0);
 
+    // Calculate the ocean color by mixing depth and surface color based on elevation and color settings
     vec3 oceanColor = mix(uDepthColor, uSurfaceColor, (vElevation + uColorOffset) * uColorMultiplier);
+
+    // Mix the ocean color with the fog color based on the fogFactor, using smoothstep for a gradual transition
     vec3 finalColor = mix(oceanColor, uFogColor, smoothstep(0.7, 1.0, fogFactor) * uFogIntensity);
 
+    // Set the final color for the fragment
     gl_FragColor = vec4(finalColor, 1.0);
 
     #include <colorspace_fragment>
--- src/shaders/Ocean/Vertex.glsl
+++ src/shaders/Ocean/Vertex.glsl
@@ -6,6 +6,7 @@ uniform float uSmallWavesElevation;
 uniform float uSmallWavesFrequency;
 uniform float uSmallWavesSpeed;
 uniform float uSmallIterations;
+
 varying float vElevation;
 varying vec3 vWorldPosition;
 varying vec2 vUv;
@@ -14,8 +15,10 @@ varying vec2 vUv;
 
 void main()
 {
+    // Set the UV coordinates to varyings
     vUv = uv;
 
+    // Calculate world position by transforming the model position by the model matrix
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     vWorldPosition = modelPosition.xyz;
 
@@ -26,16 +29,22 @@ void main()
 
     for(float i = 1.0; i <= uSmallIterations; i++)
     {
+        // Add small wave elevation by applying Perlin noise in iterations
         elevation -= abs(cnoise(vec3(modelPosition.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i);
     }
 
+    // Update the y-coordinate of the model position based on calculated elevation
     modelPosition.y += elevation;
 
+    // Apply view matrix to model position to get view space position
     vec4 viewPosition = viewMatrix * modelPosition;
+
+    // Apply projection matrix to view position for clip space coordinates
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
+    // Set final position for vertex shader output
     gl_Position = projectedPosition;
 
-    // Varyings
+    // Pass the elevation to the fragment shader
     vElevation = elevation;
 }
